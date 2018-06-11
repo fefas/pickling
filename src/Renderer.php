9298c82dedc5ce7259aaffa20322ed2e4c4bff66
Felipe Martins <me@fefas.net>
2018-06-12 01:13:45 +0300

    Render homepage using twig

--- src/Renderer.php
+++ src/Renderer.php
@@ -2,20 +2,61 @@
 
 namespace Pickling;
 
+use Twig_Loader_Array;
+use Twig_Environment;
+
 class Renderer
 {
+    /** @var string */
+    private $featuresDir;
+
+    public function __construct(string $featuresDir)
+    {
+        $this->featuresDir = $featuresDir;
+    }
+
     public function homepage(): string
     {
-        return <<<BLA
-<!DOCTYPE>
+        $template = <<<BLA
 <html>
   <body>
     <h1>Features:</h1>
     <ul>
-      <li>Some feature</li>
+    {% for feature in features %}
+      <li>{{ feature }}</li>
+    {% endfor %}
     </ul>
   </body>
 </html>
 BLA;
+
+        $loader = new Twig_Loader_Array([
+            'homepage' => $template,
+        ]);
+
+        $twig = new Twig_Environment($loader);
+
+        return $twig->render('homepage', [
+            'features' => $this->findFeatures()
+        ]);
+    }
+
+    private function findFeatures(): array
+    {
+        $resources = scandir($this->featuresDir);
+
+        $features = [];
+        foreach ($resources as $resource) {
+            if (substr($resource, -8) !== '.feature') {
+                continue;
+            }
+
+            $feature = substr($resource, 0, -8);
+            $feature = str_replace('-', ' ', $feature);
+
+            $features[] = ucfirst($feature);
+        }
+
+        return $features;
     }
 }
